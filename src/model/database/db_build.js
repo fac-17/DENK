8ebac500fb6179897f9e470eebf1a2e43463e95c
Jan Skowronski <crianonim@gmail.com>
2019-08-21 12:36:25 +0100

    Setup database

---
+++ src/model/database/db_build.js
@@ -0,0 +1,9 @@
+const dbConnection = require('./db_connection');
+const fs = require('fs');
+const path = require('path');
+
+const buildLink = path.join(__dirname, 'db_build.sql');//creating a path theat is a string 
+const sql = fs.readFileSync(buildLink).toString();//listing of sql we create
+
+const runBuild = cb =>dbConnection.query(sql, cb);
+module.exports = runBuild;
---
+++ src/model/database/db_build.sql
@@ -0,0 +1,66 @@
+BEGIN;
+
+DROP TABLE IF EXISTS login_details CASCADE;
+DROP TABLE IF EXISTS svg CASCADE;
+DROP TABLE IF EXISTS shape CASCADE;
+DROP TABLE IF EXISTS svg_shape CASCADE;
+
+CREATE TABLE login_details (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(50) NOT NULL UNIQUE,
+  password VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE svg (
+    id SERIAL PRIMARY KEY,
+    props VARCHAR(200) NOT NULL,
+    name VARCHAR(40) NOT NULL UNIQUE,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES login_details(id)
+);
+
+CREATE TABLE shape (
+    id SERIAL PRIMARY KEY,
+    type VARCHAR(30) NOT NULL,
+    name VARCHAR(40) NOT NULL UNIQUE,
+    props VARCHAR(200) NOT NULL
+);
+-- click add to artwork
+CREATE TABLE svg_shape (
+    id SERIAL PRIMARY KEY,
+    svg_id INTEGER NOT NULL,
+    shape_id INTEGER NOT NULL,
+    FOREIGN KEY (svg_id) REFERENCES svg(id),
+    FOREIGN KEY (shape_id) REFERENCES shape(id)
+);
+-- //login table 
+INSERT INTO login_details(username, password) VALUES
+  ('LEONIE', '$2b$10$iYJ0/hBnkJhNFWZuBTmma.Yxwugo7uTXn/cp9sxzEG4HKI4D72fcu'),
+  ('JAN', '$2b$10$iYJ0/hBnkJhNFWZuBTmma.RcgZHxWjFm.rVEm3Fz1kaKV4/Zz76HC'),
+  ('FRANCESCA', '2b$10$iYJ0/hBnkJhNFWZuBTmma..9LfCAylv8Pel9Xjx3jS3IIzmfHneBm'),
+  ('COLETTE', '$2b$10$iYJ0/hBnkJhNFWZuBTmma.rdnUme6fFMbFk/vzYaz2wv6HzrLH7dy'),
+  ('SAM', '$2b$10$iYJ0/hBnkJhNFWZuBTmma.UU02CTZN5dgtQ54ZnIFOKyolV.1ZyKO');
+
+INSERT INTO svg(name,props,user_id) VALUES
+  ('picasso','{"fill":"pink"}',1),
+  ('rembrandt','{"stroke":"red"}',2),
+  ('banksy','{"viewPort":"0 0 72 72"}',3),
+  ('monet','{"viewPort":"0 0 72 72"}',4),
+  ('dali','{"viewPort":"0 0 72 72"}',5);
+
+INSERT INTO shape(name,type,props) VALUES
+('circle1','circle','{"cy":23,"cx":34,"r":10}'),
+('sq','rect','{"x":20,"y":30,"width":40,"height":40}'),
+('triangle','polygon','{"points":"60 60 30 30 10 50"}'),
+('hex','polygon','{"points":"36 4 52 20 52 36 36 70 20 36"}'),
+('ring','circle','{"cx":50,"cy":70,"r":30}');
+
+INSERT INTO svg_shape(svg_id,shape_id) VALUES
+(1,3),
+(1,5),
+(2,5),
+(2,3),
+(2,1),
+(3,4);
+
+COMMIT;
