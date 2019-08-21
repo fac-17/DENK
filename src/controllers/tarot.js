840f0fa590a050516b92a12b20150bb4881f02c8
Jan Skowronski <crianonim@gmail.com>
2019-08-21 16:18:57 +0100

    Create query for random card

--- src/controllers/tarot.js
+++ src/controllers/tarot.js
@@ -1,3 +1,8 @@
+const getRandomCardQuery = require('../model/database/queries/getRandomCard')
 exports.tarot = (req, res) => {
+  console.log({getRandomCardQuery})
+getRandomCardQuery().then(result=>{
+  console.log(result.rows)
+})
   res.render('tarot');
 };
