13c84d5e070b475cf17ae776932f7d2ced23de4d
Christine <christine.altmann@gmx.com>
2019-08-22 11:47:28 +0100

    tests for the two different routes

--- tests/router.test.js
+++ tests/router.test.js
@@ -6,7 +6,27 @@ test("set up works", t => {
   request(app)
     .get("/")
     .expect(200)
-    .end(function(err, res) {
+    .end((err, res) => {
+      t.equal(1, 1);
+      t.end();
+    });
+});
+
+test("Home should return the expected results", t => {
+  request(app)
+    .get("/")
+    .expect("Content-Type", /html/)
+    .end((err, res) => {
+      t.equal(1, 1);
+      t.end();
+    });
+});
+
+test("Tarot should return the expected results", t => {
+  request(app)
+    .get("/tarot")
+    .expect("Content-Type", /html/)
+    .end((err, res) => {
       t.equal(1, 1);
       t.end();
     });
