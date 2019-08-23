1b8a7148694f6887a80165c6621d650cda7dbd57
Christine <christine.altmann@gmx.com>
2019-08-23 11:36:08 +0100

    fixes consolelogs

--- public/js/main.js
+++ public/js/main.js
@@ -7,8 +7,6 @@ window.onload = () => {
     tarotHand.classList.add("hand-grow");
 }
 
-
-
 const getRandomColor = () => {
     return "rgb(" + [0, 0, 0].map(getRandomByte).join(",") + ")";
 }
@@ -16,7 +14,6 @@ const getRandomColor = () => {
 const getRandomByte = () => {
     return Math.floor(Math.random() * 256)
 }
-console.log(document.querySelector(".svg-sunmiddle.crescent"));
 
 const cardClickHandler = () => {
     location.href = "/tarot"
@@ -25,14 +22,10 @@ const cardClickHandler = () => {
 document.querySelectorAll(".tarot-card-svg").forEach(svg => {
     svg.addEventListener("click", cardClickHandler)
     classes.forEach(c => {
-        console.log(svg, "this is svg")
-        console.log(c, 'this is c')
         try {
             svg.querySelector(c).setAttribute("fill", getRandomColor())
-            console.log("WO")
         }
         catch (e) {
-            console.log("DIDN")
         }
     })
 })
